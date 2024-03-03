3fda2581e852d904dce548b56b3363912681bd7e
RoroSin <rezbogd@gmail.com>
2024-03-03 14:38:05 +0300

    memory usage of vector

--- libs/vector.h
+++ libs/vector.h
@@ -1,4 +1,3 @@
-
 #ifndef C_VECTOR_H
 #define C_VECTOR_H
 
@@ -13,5 +12,51 @@ typedef struct vector {
     size_t size;
     size_t capacity;
 } vector;
+
+void canFit(void* x){
+    if(x == NULL){
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+}
+
+vector createVector(size_t n) {
+    vector v;
+    v.size = 0;
+    v.capacity = n;
+    v.data = realloc(v.data, n * sizeof(int));
+
+    canFit(v.data);
+
+    return v;
+}
+
+void reserve(vector *v, size_t newCapacity){
+    v -> capacity = newCapacity;
+
+    if(v -> size < newCapacity)
+        v -> size = newCapacity;
+
+    if(newCapacity == 0)
+        v -> data = NULL;
+    else {
+        v->data = realloc(v->data, newCapacity * sizeof(int));
+        canFit(v->data);
+    }
+}
+
+void clear(vector *v){
+    v -> size = 0;
+}
+
+void shrinkToFit(vector *v){
+    v -> capacity = v -> size;
+}
+
+void deleteVector(vector *v){
+    free(v -> data);
+    v -> size = 0;
+    v -> capacity = 0;
+}
 
 #endif //C_VECTOR_H
