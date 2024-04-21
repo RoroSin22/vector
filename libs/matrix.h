553dd1c063b293410e857eb8c32226067b192660
RoroSin <rezbogd@gmail.com>
2024-04-21 20:28:17 +0300

    square matrix functions

--- libs/matrix.h
+++ libs/matrix.h
@@ -57,8 +57,8 @@ void inputMatrices(matrix *ms, int nMatrices){
 }
 
 void outputMatrix(matrix m){
-    for (int i = 0; i < m.nCols; i++){
-        for (int j = 0; j < m.nRows; j++)
+    for (int i = 0; i < m.nRows; i++){
+        for (int j = 0; j < m.nCols; j++)
             printf("%d ", m.values[i][j]);
         printf("\n");
     }
@@ -83,5 +83,99 @@ void swapColumns(matrix m, int j1, int j2){
     }
 }
 
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)){
+    for (int i = 1; i < m.nRows ; i++){
+        int *temp = m.values[i];
+        int j = i;
+
+        while (j > 0 && criteria(m.values[j - 1], m.nCols) > criteria(temp, m.nCols)) {
+            m.values[j] = m.values[j - 1];
+            j--;
+        }
+
+        m.values[j] = temp;
+    }
+}
+
+/*int getTempCols(matrix m, int cols){
+    int *temp = (int *) malloc(sizeof(int) * m.nCols);
+    for (int i = 0; i < m.nCols; i++)
+        temp[i] = m.values[cols] [i];
+
+    return *temp;
+}
+
+void switchCols(matrix *m, int col_number, int *second_col){
+    for (int i = 0; i < m->nRows; i++)
+        m->values[i] [col_number] = second_col[i];
+}
+
+void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int)){
+    for (int i = 0; i < m.nCols; i++){
+        int temp = getTempCols(m, i);
+        int j = i;
+
+        while (j > 0 && criteria(m.values[j - 1], m.nCols) > criteria(&temp, m.nCols)) {
+            switchCols(m, j, m.values[])
+            j--;
+        }
+    }
+} */
+
+bool isSquareMatrix(matrix *m){
+    return m->nCols == m->nRows;
+}
+
+bool areTwoMatricesEqual(matrix *m1, matrix *m2){
+    if (m1->nCols != m2->nCols && m1->nRows != m2->nRows)
+        return false;
+
+    for (int i = 0; i < m1->nRows; i++){
+        if (memcmp(m1->values[i], m2->values[i], sizeof(int) * m1->nCols) != 0)
+            return false;
+    }
+
+    return true;
+}
+
+bool onlyNotZeroAtRow(int *r, int length, int position){
+    for(int i = 0; i < length; i++){
+        if(r[i] != 0 && i != position)
+            return false;
+    }
+
+    return true;
+}
+
+bool isEMatrix(matrix *m){
+    if (!isSquareMatrix(m))
+        return 0;
+
+    for (int i = 0; i < m->nRows; i++){
+        if (!onlyNotZeroAtRow(m->values[i], m->nCols, i))
+            return false;
+    }
+
+    return true;
+}
+
+bool isSymmetricMatrix(matrix *m){
+    if (!isSquareMatrix(m))
+        return false;
+    if (m->nRows == 1)
+        return true;
+
+    for (int i = 0; i < m->nRows / 2 + m->nRows % 2; i++){
+        for (int j = 0; j < m->nCols / 2 + m->nCols % 2; j++) {
+            if (i == j)
+                continue;
+
+            if (m->values[i][j] != m->values[j][i])
+                return false;
+        }
+    }
+
+    return true;
+}
 
 #endif //C_MATRIX_H
--- main.c
+++ main.c
@@ -2,11 +2,27 @@
 #include <stdbool.h>
 #include "libs/matrix.h"
 
+int getSum(int *a, int n){
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += a[i];
+
+    return sum;
+}
+
+int getDifference(int *a, int n){
+    int difference = 0;
+    for (int i = 0; i < n; i++)
+        difference -= a[i];
+
+    return difference;
+}
+
 int main() {
     matrix a = getMemMatrix(3, 3);
     inputMatrix(&a);
-    swapColumns(a, 0, 2);
-    outputMatrix(a);
+
+    printf("%d", isSymmetricMatrix(&a));
 
     return 0;
 }
