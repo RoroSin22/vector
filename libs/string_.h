fc89989ae1ea5f408b2b0329b81637b22f7dbda9
RoroSin <rezbogd@gmail.com>
2024-05-04 13:46:38 +0300

    findNonSpace with test

--- libs/string_.h
+++ libs/string_.h
@@ -1,8 +1,28 @@
-//
-// Created by Bogdan on 04.05.2024.
-//
-
 #ifndef C_STRING__H
 #define C_STRING__H
+
+#include <ctype.h>
+
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+
+    return end - begin;
+}
+
+char* find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+
+    return begin;
+}
+
+char* findNonSpace(char *begin){
+    while (*begin != '\0' && isspace(*begin))
+        begin++;
+
+    return begin;
+}
 
 #endif //C_STRING__H
