d1eae3f30ab4f7238067bf1ed61d757e6845764c
RoroSin <rezbogd@gmail.com>
2024-05-18 02:50:48 +0300

    task 2 with test

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include "libs/matrix.h"
 #include <assert.h>
 #include <time.h>
+#include "libs/vectorVoid.h"
+
+
+bool fequal(float a, float b){
+    return fabs(a-b) < DBL_EPSILON;
+}
 
 //1
 
@@ -155,18 +161,92 @@ void test1_3(){
     freeMemMatrix(&m_res);
 }
 
-
 void test1(){
     test1_1();
     test1_2();
     test1_3();
 }
 
+//2
+void convert_float(const char* file_name) {
+    FILE* file = fopen(file_name, "r");
+
+    vectorVoid v = createVectorV(0, sizeof(float));
+
+    while (!feof(file)) {
+        float x;
+        fscanf(file, "%f", &x);
+        pushBackV(&v, &x);
+    }
+
+    fclose(file);
+
+    file = fopen(file_name, "w");
+
+    for (size_t i = 0; i < v.size; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+        fprintf(file, "%.2f ", x);
+    }
+
+    deleteVectorV(&v);
+    fclose(file);
+}
+
+void test_convert_float(){
+    const char file_name[] = "aboba";
+
+    vectorVoid v = createVectorV(3, sizeof(float));
+    float number1 = 7.143423;
+    pushBackV(&v, &number1);
+    float number2 = 2.241518;
+    pushBackV(&v, &number2);
+    float number3 = 9.353738;
+    pushBackV(&v, &number3);
+
+    FILE* file = fopen(file_name, "w");
+
+    for (size_t i = 0; i < v.size; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+        fprintf(file, "%.2f ", x);
+    }
+
+    fclose(file);
+
+    convert_float(file_name);
+
+    vectorVoid v_exp = createVectorV(3, sizeof(float));
+    float number1_exp = 7.143423;
+    pushBackV(&v_exp, &number1_exp);
+    float number2_exp = 2.241518;
+    pushBackV(&v_exp, &number2_exp);
+    float number3_exp = 9.353738;
+    pushBackV(&v_exp, &number3_exp);
+
+    file = fopen(file_name, "r");
+
+    for (int i = 0; i < 3; i++) {
+        float y;
+        float y_exp;
+        fscanf(file, "%f\n", &y);
+        getVectorValueV(&v_exp, sizeof(float), &y_exp);
+        assert(fequal(y, y_exp) == 0);
+    }
+
+    fclose(file);
+
+    deleteVectorV(&v);
+    deleteVectorV(&v_exp);
+}
+
 void test(){
     test1();
+    test_convert_float();
 }
 
 int main() {
     test();
+
     return 0;
 }
