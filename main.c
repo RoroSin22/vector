fc934502a15fbebf37228925534a23b877415e8b
RoroSin <rezbogd@gmail.com>
2024-05-25 18:49:35 +0300

    task 2; gameOfLife with test

--- main.c
+++ main.c
@@ -19,9 +19,8 @@ matrix addOnes(int n, int* query[], int operations){
         int end_col = query[i][3];
 
         for (int j = begin_row; j <= end_row; j++){
-            for (int k = begin_col; k <= end_col; k++) {
+            for (int k = begin_col; k <= end_col; k++)
                 m.values[j][k] += 1;
-            }
         }
     }
 
@@ -41,8 +40,63 @@ void test_task1(){
     assert(areTwoMatricesEqual(&res, &exp));
 }
 
+//2
+int countNeighbours(matrix m, int index_row, int index_col){
+    int begin_row = index_row - 1 >= 0 ? index_row - 1 : index_row;
+    int begin_col = index_col - 1 >= 0 ? index_col - 1 : index_col;
+    int end_row = index_row + 1 < m.nRows ? index_row + 1 : index_row;
+    int end_col = index_col + 1 < m.nCols ? index_col + 1 : index_col;
+
+    int counter = 0;
+
+    for (int j = begin_row; j <= end_row; j++) {
+        for (int k = begin_col; k <= end_col; k++)
+            counter += m.values[j][k];
+    }
+
+    return counter - m.values[index_row][index_col];
+}
+
+void gameOfLife(matrix m){
+    matrix temp = getMemMatrix(m.nRows, m.nCols);
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++)
+            temp.values[i][j] = m.values[i][j];
+    }
+
+    for (int i = 0; i < m.nRows; i++){
+        for (int j = 0; j < m.nCols; j++){
+            int neighbours = countNeighbours(temp, i, j);
+            if (neighbours < 2 || neighbours > 3)
+                m.values[i][j] = 0;
+            else if (neighbours == 3)
+                m.values[i][j] = 1;
+        }
+    }
+
+    freeMemMatrix(&temp);
+}
+
+void test_task2(){
+    matrix m = createMatrixFromArray((int[]) {
+                                             0, 1, 0,
+                                             0, 0, 1,
+                                             1, 1, 1,
+                                             0, 0, 0},
+                                     4, 3);
+    matrix res = createMatrixFromArray((int[]) {
+                                             0, 0, 0,
+                                             1, 0, 1,
+                                             0, 1, 1,
+                                             0, 1, 0},
+                                     4, 3);
+    gameOfLife(m);
+    assert(areTwoMatricesEqual(&m, &res));
+}
+
 void test(){
     test_task1();
+    test_task2();
 }
 
 int main() {
