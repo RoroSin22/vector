824767aca8995862ad7146cb58b929ed571de8d5
RoroSin <rezbogd@gmail.com>
2024-06-11 10:54:12 +0300

    exam; MatrixCanBeEqual

--- main.c
+++ main.c
@@ -34,7 +34,103 @@ void testAreLinesEqual(){
     testAreLinesEqual_3();
 }
 
+bool matrixCanBeEqual(matrix m1, matrix m2){
+    int matrix_size = m1.nRows;
+    bool exeption_array[matrix_size];
+    for (int i = 0; i < matrix_size; i++)
+        exeption_array[i] = 1;
+
+    for(int i = 0; i < matrix_size; i++){
+        for(int j = 0; j < matrix_size; j++){
+            if (exeption_array[j] == 0)
+                continue;
+            if (areLinesEqual(m1.values[i], m2.values[j], matrix_size)){
+                exeption_array[j] = 0;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < matrix_size; i++){
+        if (exeption_array[i] == 1)
+            return 0;
+    }
+
+    return 1;
+}
+
+void testMatrixCanBeEqual1(){
+    matrix m1 = createMatrixFromArray((int[]){1, 2, 3,
+                                              7, 8, 9,
+                                              4, 5, 6}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]){1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 9}, 3, 3);
+
+    assert(matrixCanBeEqual(m1, m2) == 1);
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void testMatrixCanBeEqual2(){
+    matrix m1 = createMatrixFromArray((int[]){}, 0, 0);
+    matrix m2 = createMatrixFromArray((int[]){}, 0, 0);
+
+    assert(matrixCanBeEqual(m1, m2) == 1);
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void testMatrixCanBeEqual3(){
+    matrix m1 = createMatrixFromArray((int[]){1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 9}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]){1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 9}, 3, 3);
+
+    assert(matrixCanBeEqual(m1, m2) == 1);
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void testMatrixCanBeEqual4(){
+    matrix m1 = createMatrixFromArray((int[]){1, 2, 3,
+                                              7, 8, 9,
+                                              4, 5, 6}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]){1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 9}, 3, 3);
+
+    assert(matrixCanBeEqual(m1, m2) == 1);
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void testMatrixCanBeEqual5(){
+    matrix m1 = createMatrixFromArray((int[]){1, 2, 3,
+                                              7, 8, 9,
+                                              4, 5, 6}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]){1, 2, 3,
+                                              4, 3, 6,
+                                              7, 2, 9}, 3, 3);
+
+    assert(matrixCanBeEqual(m1, m2) == 0);
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void testMatrixCanBeEqual(){
+    testMatrixCanBeEqual1();
+    testMatrixCanBeEqual2();
+    testMatrixCanBeEqual3();
+    testMatrixCanBeEqual4();
+    testMatrixCanBeEqual5();
+}
+
+
 int main() {
-    testAreLinesEqual();
+    testMatrixCanBeEqual();
+
     return 0;
 }
